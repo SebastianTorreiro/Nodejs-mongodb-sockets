0178da0545242beb57dfeeb318b3b8a9f619393e
Sebastian Torreiro <sebastiantorreiro@gmail.com>
2023-03-09 17:07:02 -0300

    preset socket.io & connect mongodb

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-import { Express } from "express";
-console.log("ashe")
+import app from './app'
+import { Server as websocketServer } from 'socket.io'
+import http from 'http'
+
+import { connectDB } from './db'
+
+connectDB();
+
+const server = http.createServer(app)
+const httpServer = server.listen(3000)
+const io = new websocketServer(httpServer)
+
+console.log('server is running on port 3000')
+
