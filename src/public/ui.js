afd78f3a59aa2a34bc2adbaf43e3e3c471758c6a
Sebastian Torreiro <sebastiantorreiro@gmail.com>
2023-03-15 23:48:42 -0300

    boostrap

--- src/public/ui.js
+++ src/public/ui.js
@@ -1,58 +1,61 @@
-import {saveNotes, deleteNote, getNoteById, updateNote} from './socket.js'
+import { saveNotes, deleteNote, getNoteById, updateNote } from "./socket.js";
 
-const notesList = document.querySelector('#notes');
+const notesList = document.querySelector("#notes");
 
-const title = document.querySelector('#title')
-const description = document.querySelector('#description')
+const title = document.querySelector("#title");
+const description = document.querySelector("#description");
 
-let saveId = '';
+let saveId = "";
 
-const noteUI = note =>{
-  const div = document.createElement('div')
+const noteUI = (note) => {
+  const div = document.createElement("div");
   div.innerHTML += `
-  <div>
-    <h1>${note.title}</h1>
+  <div class="card card-body rounded-0 mb-2 animate__animated animate__fadeInUp">
+    <div class="d-flex justify-content-between"> 
+     <h1>${note.title}</h1>
+      <div>
+        <button class="btn btn-danger btn-sm delete" data-id="${note._id}">Delete</button>
+        <button class="btn btn-secondary btn-sm update" data-id="${note._id}">Update</button>
+      </div>
+    </div>
     <p>${note.description}</p>
-    <button class="delete" data-id="${note._id}">Delete</button>
-    <button class="update" data-id="${note._id}">Update</button>
-  </div>
-  `
-  const btnDelete = div.querySelector(".delete")
-  const btnupdate = div.querySelector('.update')
   
-  btnDelete.addEventListener("click", () => deleteNote(btnDelete.dataset.id))
-  btnupdate.addEventListener('click', () => getNoteById(btnupdate.dataset.id))
+  </div>
+  `;
+  const btnDelete = div.querySelector(".delete");
+  const btnupdate = div.querySelector(".update");
 
-  return div
-}
+  btnDelete.addEventListener("click", () => deleteNote(btnDelete.dataset.id));
+  btnupdate.addEventListener("click", () => getNoteById(btnupdate.dataset.id));
 
+  return div;
+};
 
-export const renderNotes = notes =>{
-  notesList.innerHTML = '';
-  notes.forEach(note => { notesList.append(noteUI(note))});
- 
+export const renderNotes = (notes) => {
+  notesList.innerHTML = "";
+  notes.forEach((note) => {
+    notesList.append(noteUI(note));
+  });
+};
+export const fillForm = (note) => {
+  title.value = note.title;
+  description.value = note.description;
+  saveId = note._id;
+};
 
-}
-export const fillForm = note =>{
-  title.value = note.title
-  description.value = note.description
-  saveId = note._id
-
-}
-
-export const onHandleSubmit = (e) =>{
+export const onHandleSubmit = (e) => {
   e.preventDefault();
 
-    if(saveId){
-      updateNote(saveId, title.value, description.value);
-    }else{
+  if (saveId) {
+    updateNote(saveId, title.value, description.value);
+  } else {
     saveNotes(title.value, description.value);
-    }
-    saveId= '';
-    title.value = '';
-    description.value= '';
-}
+  }
+  saveId = "";
+  title.value = "";
+  description.value = "";
+};
 
-export const appendNote = (note) =>{
-  notesList.append(noteUI(note))
-}
+export const appendNote = (note) => {
+  notesList.append(noteUI(note));
+};
